0827eb4b81c130f1ad3f4a86162adbb3cbd9146b
joonhyunpark <channelsucre@gmail.com>
2022-06-29 16:58:27 +0900

    Test 1

--- app/src/main/java/com/example/cs496week1/MainActivity.java
+++ app/src/main/java/com/example/cs496week1/MainActivity.java
@@ -39,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                // Test message, please ignore.
             }
         });
     }
