5d513ce75e47b73fcbebd5c0bd679a710765ae7e
hermioneee2 <hermioneee2@gmail.com>
2022-06-30 22:12:32 +0900

    TAB1 Combined (working!)

--- app/src/main/java/com/example/cs496week1/ui/main/SectionsPagerAdapter.java
+++ app/src/main/java/com/example/cs496week1/ui/main/SectionsPagerAdapter.java
@@ -2,6 +2,7 @@ package com.example.cs496week1.ui.main;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
 import androidx.fragment.app.Fragment;
@@ -10,37 +11,42 @@ import androidx.fragment.app.FragmentPagerAdapter;
 
 import com.example.cs496week1.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A [FragmentPagerAdapter] that returns a fragment corresponding to
  * one of the sections/tabs/pages.
  */
 public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
-    @StringRes
-    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
-    private final Context mContext;
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+    private final List<String> mFragmentTitleList = new ArrayList<>();
 
-    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+    public SectionsPagerAdapter(@NonNull FragmentManager fm) {
         super(fm);
-        mContext = context;
     }
+//
+////    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+////        super(fm);
+////        mContext = context;
+////    }
 
-    @Override
-    public Fragment getItem(int position) {
-        // getItem is called to instantiate the fragment for the given page.
-        // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1);
+    public void addFragment(Fragment fragment, String title){
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
     }
-
-    @Nullable
-    @Override
     public CharSequence getPageTitle(int position) {
-        return mContext.getResources().getString(TAB_TITLES[position]);
+        return mFragmentTitleList.get(position);
     }
+    //    public SectionPageAdapter(FragmentManager fm){
+//        super(fm);
+//    }
+    @Override
+    public Fragment getItem(int position) { return mFragmentList.get(position); }
 
     @Override
     public int getCount() {
-        // Show 2 total pages.
-        return 2;
+        return mFragmentList.size() ;
     }
 }
